52c0326328ac75fa12515ca015bb5927c76db754
ScubaScott <scott.reiling@gmail.com>
2014-01-04 20:24:08 -0600

    example scheduled job from thermostat

--- SYSTEM_THERMOSTAT_thermostat.js
+++ SYSTEM_THERMOSTAT_thermostat.js
@@ -5,7 +5,12 @@ var definition = {
 		{
 			triggerType : "SCHEDULED",
 			schedules : [
-							]
+									{
+						eventName : "weekday morning1",
+						schedule : "0 0 6 * * 1,2,3,4,5",
+						timeZone : "US/Central"
+					}
+			]
 		}
 	]
 };
@@ -15,6 +20,17 @@ function run(context){
 	
 	switch (eventName) {
 		
+		case "weekday morning1":
+		
+			context.sendNodeCommand("thermostat", 'Thermostat', {
+				async: false,
+				mode:'HEAT',
+				fanmode:'AUTO',
+				heatpoint:18.5,
+				coolpoint:26.5
+			});
+
+		break;
 		
 		default:
 			logger.info("Unknown event [" + eventName + "].  Cannot execute.");
